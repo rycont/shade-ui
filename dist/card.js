630f16388c5b531231f896ba153b2679850618fb
rycont <rycont>
2024-05-06 06:07:24 +0000

    build: card component

---
+++ dist/card.js
@@ -0,0 +1,17 @@
+import { D as e } from "./util-BfafNPJz.js";
+import "./theme.css.ts.vanilla-l0sNRNKZ.js";
+var t = "sh-ui-1f7zqr50";
+class s extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.setAttribute("role", "card"), this.classList.add(t);
+  }
+}
+const r = "sh-card";
+e.define(r, s);
+export {
+  r as default
+};
+//# sourceMappingURL=card.js.map
