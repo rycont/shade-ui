d4c783c46cbf2c8568656d8201758ac1e6dea720
rycont <rycont>
2024-05-06 05:22:49 +0000

    feat: add build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,32 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-export default {
-	plugins: [vanillaExtractPlugin()],
-}
+import { glob } from 'glob'
+
+const elements = await glob('./elements/*/index.ts')
+
+const elementEntries = Object.fromEntries(
+	elements.map((path) => [path.slice(9, -9), resolve(path)]),
+)
+
+export default defineConfig({
+	plugins: [
+		vanillaExtractPlugin({
+			unstable_mode: 'emitCss',
+			identifiers(props) {
+				return 'sh-ui-' + props.hash
+			},
+		}),
+	],
+	publicDir: './icons',
+	build: {
+		lib: {
+			entry: elementEntries,
+			name: 'shade-ui',
+			formats: ['es'],
+		},
+		sourcemap: true,
+		copyPublicDir: true,
+	},
+})
