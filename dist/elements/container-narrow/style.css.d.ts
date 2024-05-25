72cfc697820908b1eddeea707db9d5b3fc83cb33
rycont <rycont>
2024-05-25 11:16:29 +0000

    feat: expose narrow container width to css var

--- dist/elements/container-narrow/style.css.d.ts
+++ dist/elements/container-narrow/style.css.d.ts
@@ -1,1 +1,2 @@
+export declare const NARROW_CONTAINER_WIDTH = "--sh-narrow-container-width";
 export declare const container: string;
--- elements/container-narrow/style.css.ts
+++ elements/container-narrow/style.css.ts
@@ -4,13 +4,15 @@ import { vars } from '../../theme.css'
 const BACKGROUND_COLOR = `var(--container-background-color, ${vars.color.L1})`
 const BACKDROP_COLOR = `var(--container-backdrop-color, ${vars.color.L2})`
 
+export const NARROW_CONTAINER_WIDTH = '--sh-narrow-container-width'
+
 export const container = style({
 	backgroundColor: BACKGROUND_COLOR,
 	minHeight: '100vh',
 	display: 'flex',
 	flexDirection: 'column',
 	gap: '4rem',
-	maxWidth: '540px',
+	maxWidth: `var(${NARROW_CONTAINER_WIDTH})`,
 	margin: '0px auto',
 	padding: '6rem',
 	boxSizing: 'border-box',
@@ -19,4 +21,10 @@ export const container = style({
 
 globalStyle('body', {
 	backgroundColor: BACKDROP_COLOR,
+})
+
+globalStyle(':root', {
+	vars: {
+		[NARROW_CONTAINER_WIDTH]: '540px',
+	},
 })
